5b45c47a7b99cdbb2f35e9d70d711750eb27c2c0
uupaa <uupaa.js@gmail.com>
2015-01-19 09:49:45 +0900

    follow new WebModule test spec

--- test/testcase.js
+++ test/testcase.js
@@ -1,61 +1,76 @@
 var ModuleTestFoo = (function(global) {
 
 var _isNodeOrNodeWebKit = !!global.global;
-var _runOnNodeWebKit =  _isNodeOrNodeWebKit && /native/.test(setTimeout);
+var _runOnNodeWebKit =  _isNodeOrNodeWebKit &&  /native/.test(setTimeout);
 var _runOnNode       =  _isNodeOrNodeWebKit && !/native/.test(setTimeout);
 var _runOnWorker     = !_isNodeOrNodeWebKit && "WorkerLocation" in global;
 var _runOnBrowser    = !_isNodeOrNodeWebKit && "document" in global;
 
-return new Test("Foo", {
-        disable:    false,
-        browser:    true,
-        worker:     true,
-        node:       true,
-        nw:         true,
-        button:     true,
-        both:       true, // test the primary module and secondary module
+var test = new Test("Foo", {
+        disable:    false, // disable all tests.
+        browser:    true,  // enable browser test.
+        worker:     true,  // enable worker test.
+        node:       true,  // enable node test.
+        nw:         true,  // enable nw.js test.
+        button:     true,  // show button.
+        both:       true,  // test primary and secondary modules.
+        ignoreError:false, // ignore error.
     }).add([
         testFoo_value,
-        testFoo_isNumber,
-        testFoo_isInteger,
-    ]).run().clone();
+        testFoo_concat,
+        testFoo_concat$,
+    ]);
+
+if (_runOnBrowser || _runOnNodeWebKit) {
+    //test.add([]);
+} else if (_runOnWorker) {
+    //test.add([]);
+} else if (_runOnNode) {
+    //test.add([]);
+}
+
+return test.run().clone();
 
 function testFoo_value(test, pass, miss) {
 
-    var result = new Foo(123.4).value();
+    var instance = new Foo("a");
 
-    if (result === 123.4) {
-        test.done(pass());
-    } else {
+    if (instance.value === "a") {
+        instance.value = "b";
+
+        if (instance.value === "b") {
+            test.done(pass());
+            return;
+        }
+    }
+    test.done(miss());
+}
+
+function testFoo_concat(test, pass, miss) {
+
+    var result = {
+            0: new Foo(   ).concat("a") === "a", // true
+            1: new Foo("b").concat("b") === "bb" // true
+        };
+
+    if (/false/.test(JSON.stringify(result, null, 2))) {
         test.done(miss());
+    } else {
+        test.done(pass());
     }
 }
 
-function testFoo_isNumber(test, pass, miss) {
+function testFoo_concat$(test, pass, miss) {
 
-    var result = [
-            new Foo(123.4).isNumber(),  // true
-            new Foo(123.0).isNumber()   // true
-        ];
+    var result = {
+            0: new Foo(   ).concat$("a").value === "a", // true
+            1: new Foo("b").concat$("b").value === "bb" // true
+        };
 
-    if (!/false/.test(result.join())) {
-        test.done(pass());
-    } else {
+    if (/false/.test(JSON.stringify(result, null, 2))) {
         test.done(miss());
-    }
-}
-
-function testFoo_isInteger(test, pass, miss) {
-
-    var result = [
-           !new Foo(123.4).isInteger(), // !false -> true
-            new Foo(123.0).isInteger()  // true
-        ];
-
-    if (!/false/.test(result.join())) {
-        test.done(pass());
     } else {
-        test.done(miss());
+        test.done(pass());
     }
 }
 
